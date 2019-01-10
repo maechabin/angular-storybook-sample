0ce1ebc9a72723ea846651a2971445e837458f28
maechabin <mitoppo@gmail.com>
2019-01-10 21:57:12 +0900

    ファイル名修正

--- src/app/app.component.stories.ts
+++ src/app/app.component.stories.ts
@@ -2,15 +2,18 @@ import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { InputComponent } from './input/input.component';
 
 storiesOf('AppComponent', module).add('to Storybook', () => ({
   component: AppComponent,
   props: {},
   moduleMetadata: {
-    declarations: [ChildComponent],
+    imports: [ReactiveFormsModule],
+    declarations: [ChildComponent, InputComponent],
   },
 }));
 
