7e28431ce6c1b6a2a543cb24b7bbed13e8547339
maechabin <mitoppo@gmail.com>
2019-01-17 21:59:48 +0900

    storyshots対応

---
+++ .storybook/config.js
@@ -0,0 +1,16 @@
+import { configure } from '@storybook/angular';
+// import requireContext from 'require-context.macro';
+
+// automatically import all files ending in *.stories.ts
+const req = require.context('../src/app', true, /.stories.ts$/);
+//const req = requireContext('../../src/app', true, /.stories.ts$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+  // setTimeout(() => {
+  //   document
+  //     .getElementsByTagName('storybook-dynamic-app-root')[0]
+  //     .classList.add('markdown-body');
+  // }, 1000);
+}
+
+configure(loadStories, module);
--- jest.config.js
+++ jest.config.js
@@ -1,49 +1,52 @@
-// module.exports = {
-//   globals: {
-//     __TRANSFORM_HTML__: true
-//     // 'ts-jest': {
-//     //   tsConfig: '<rootDir>/src/tsconfig.spec.json',
-//     //   stringifyContentPathRegex: '\\.html$',
-//     // },
-//   },
-//   transform: {
-//     "^.+\\.(ts|js|html)$": "babel-jest",
-//     // "^.+\\.jsx?$": "babel-jest",
-//     "^.+\\.(ts|html)$":
-//       "<rootDir>/node_modules/jest-preset-angular/preprocessor.js"
-//   },
-//   testMatch: [
-//     "**/__tests__/**/*.+(ts|js)?(x)",
-//     "**/?(*.)+(spec|test).+(ts|js)?(x)"
-//   ],
-//   moduleFileExtensions: ["js", "json", "jsx", "node", "ts", "tsx", "html"],
-//   moduleNameMapper: {
-//     "^src/(.*)$": "<rootDir>/src/$1",
-//     "^app/(.*)$": "<rootDir>/src/app/$1",
-//     "^assets/(.*)$": "<rootDir>/src/assets/$1",
-//     "^environments/(.*)$": "<rootDir>/src/environments/$1"
-//   },
-//   // transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
-//   snapshotSerializers: [
-//     "jest-preset-angular/AngularSnapshotSerializer.js",
-//     "jest-preset-angular/HTMLCommentSerializer.js"
-//   ],
-//   setupFiles: ["<rootDir>/.jest/register-context.js"]
-// };
-
 module.exports = {
-  cacheDirectory: "./.cache",
-  preset: "jest-preset-angular",
   globals: {
-    stringifyContentPathRegex: true
+    __TRANSFORM_HTML__: true,
+    // 'ts-jest': {
+    //   tsConfig: '<rootDir>/src/tsconfig.spec.json',
+    //   stringifyContentPathRegex: '\\.html$',
+    // },
   },
   transform: {
-    "^.+\\.(ts|js|html)$": "ts-jest",
-    "^.+\\.(ts|html)$":
-      "<rootDir>/node_modules/jest-preset-angular/preprocessor.js"
+    '^.+\\.(ts|js|html)$': 'babel-jest',
+    // "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.(ts|html)$':
+      '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", ".html"],
-  setupTestFrameworkScriptFile: "<rootDir>/src/setupJest.ts",
-  setupFiles: ["<rootDir>/.jest/register-context.js"],
-  transformIgnorePatterns: ["node_modules"]
+  testMatch: [
+    '**/__tests__/**/*.+(ts|js)?(x)',
+    '**/?(*.)+(spec|test).+(ts|js)?(x)',
+  ],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx', 'html'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+  },
+  // transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js',
+  ],
+  setupFiles: ['<rootDir>/.jest/register-context.js'],
 };
+
+// module.exports = {
+//   cacheDirectory: './.cache',
+//   preset: 'jest-preset-angular',
+//   globals: {
+//     stringifyContentPathRegex: true,
+//     'ts-jest': {
+//       tsConfig: 'src/tsconfig.spec.json',
+//     },
+//   },
+//   transform: {
+//     '^.+\\.(ts|js|html)$': 'ts-jest',
+//     '^.+\\.(ts|html)$':
+//       '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
+//   },
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', '.html'],
+//   setupTestFrameworkScriptFile: '<rootDir>/src/setupJest.ts',
+//   setupFiles: ['<rootDir>/.jest/register-context.js'],
+//   transformIgnorePatterns: ['node_modules'],
+// };
